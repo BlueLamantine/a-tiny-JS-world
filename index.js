c292b70564b2c62d930c8dad4ad01bc9b089e061
Viktoriia <urban.03al03@gmail.com>
2020-12-06 14:54:35 +0200

    post-oop added

--- index.js
+++ index.js
@@ -4,103 +4,121 @@
    Code repository: https://github.com/BlueLamantine/a-tiny-JS-world
    Web app: https://bluelamantine.github.io//a-tiny-JS-world/
    */
+class General {
 
-class Pets {
-
-   constructor (props){
-       this.species = props.species;
-       this.name = props.name;
-       this.gender = props.gender;
-       this.paws = 4;
-       this.friends =  Array.isArray(props.friends) ? props.friends : 'no friends';    
-   }
-
-   toSting () {
-      return `This ${this.species} is a ${this.gender}, its name is ${this.name}. The ${this.species} has ${this.paws} paws. Makes a sound \'${this.saund}\'. Social with : ${this.friends}`
-   }
-}
-class Cat extends Pets {
-
-   static feed = {
-       full : 'Purrrr Purrr',
-       normal : 'Mur',
-       hungry : 'Meoooow Meoooow'
-   }
-
-   constructor (props){
-       super(props);
-       this.saund = Cat.feed[props.type];
+   constructor(props) {
+      this.name = props.name;
+      this.friends = Array.isArray(props.friends) ? props.friends : 'no friends';
    }
 
 }
-class Dog extends Pets {
 
-   static mood = {
-       happy : 'Ayy-woof',
-       outdoor : 'Woof woof',
-       angry : 'Grrrrrr Grrrrrr'
+class Human extends General {
+
+   constructor(props) {
+      super(props);
+      this.species = 'human'
+      this.legs = 2;
+      this.hands = 2;
+   };
+   
+   static getGender = {
+      male : ['He', 'his'],
+      female : ['She', 'her']
+   };
+
+   getInfo() {
+      return `This ${this.species} is a ${this.gender}, ${Human.getGender[this.gender][1]} name is ${this.name}. ${Human.getGender[this.gender][0]} has ${this.legs} legs and ${this.hands} hands. ${Human.getGender[this.gender][0]} says \'${this.voise}\'. Friend with : ${this.friends}`;
    }
 
-   constructor (props){
-       super(props);
-       this.saund = Dog.mood[props.type];
-   }
+
 }
 
-const kitty = new Cat({ species : 'cat', name : 'Garfield', gender : 'male', type : 'full', friends : ['Dog', 'Dog2']});
-const doggie = new Dog({ species : 'dog', name : 'Hachiko', gender : 'male', type: 'outdoor'});
+class Man extends Human {
 
-const jsWorld = [kitty.toSting(),doggie.toSting()];
-jsWorld.forEach( el => {
-   print(el, 'p');
+   constructor(props) {
+      super(props);
+      this.gender = 'male';
+      this.voise = 'What a wonderful world!';
+   }
+   
+}
+
+class Woman extends Human {
+
+   constructor(props){
+      super(props);
+      this.gender = 'female';
+      this.voise = 'Feminism is in the air!';
+   }
+
+}
+
+class Pet extends General {
+
+   constructor(props) {
+      super(props);
+      this.paws = 4;
+   }
+
+   getInfo() {
+      return `This ${this.species} is a ${this.gender}, its name is ${this.name}. The ${this.species} has ${this.paws} paws. Makes a sound \'${this.sound}\'. Social with : ${this.friends}`;
+   }
+
+}
+
+class Cat extends Pet {
+
+   constructor(props) {
+      super(props);
+      this.species = 'cat';
+      this.gender = props.gender;
+      this.sound = 'Meow Purrrr Purrr';
+   }
+
+}
+
+class Dog extends Pet {
+
+   constructor(props) {
+     super(props);
+     this.species = 'dog';
+     this.gender = props.gender;
+     this.sound = 'Woof woof';
+   }
+
+ }
+
+class CatWoman extends Woman {
+
+   constructor(props) {
+      super(props);
+      this.species = 'cat-woman';
+      this.voise = cat.sound;
+      this.friends = props.friends;
+   }
+
+}
+
+const woman = new Woman({ name: 'Eva', friends: ['Garfield', 'Jack'] });
+
+const cat = new Cat({ name: 'Garfield', gender: 'male' });
+
+const catWoman = new CatWoman(
+   Object.assign(this, new Cat({
+      name: 'Mary',
+      gender: 'female',
+      friends : ['Eva', 'Garfield']
+    }) )
+);
+
+const dog = new Dog({
+  name: 'Hachiko',
+  gender: 'male',
+  friends: ['Eva', 'Jack']
 });
-/*const man = {
-   species: 'Human',
-   name: 'Parker',
-   gender: 'male',
-   legs: 2,
-   hands: 2,
-   saying: 'What a wonderful world!',
-   friends: ['Eva','Hachiko']
-};
- const dog = {
-   species: 'Dog',
-   name: 'Hachiko',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Woof-woof',
-   friends: ['Parker']
-};
-const woman = {
-   species: 'Human',
-   name: 'Eva',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'Feminism in the air!',
-   friends: ['Parker', 'Garfield']
-};
-const cat = {
-   species: 'Cat',
-   name: 'Garfield',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Meoooooow',
-   friends: ['Eva','Parker']
-};
-const catWoman = {
-   species: 'Supernatural',
-   name: 'Mary',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
-   friends: 'No friends'
-};
-const jsWorld = [man,woman,dog,cat,catWoman];
 
-jsWorld.forEach( el => {
-   print(Object.values(el).join(';'));
-});*/
+const man = new Man({ name: 'Jack', friends: ['Hachiko', 'Eva'] });
+
+[man, woman, cat, dog, catWoman].map(el => print(el.getInfo(), 'p'));;
+
